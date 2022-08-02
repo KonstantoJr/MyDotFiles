d3f98b18948c0eccc865a9d97148e71f83e81ed8
KonstantoJr <konstantokon29@gmail.com>
2022-08-02 03:27:18 +0300

    added keybind to call record.sh script which is a toggleable script to record a specific region on screen

---
+++ .config/sway/scripts/record.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/bash
+
+# If an instance of wf-recorder is running under this user kill it with SIGINT and exit
+pkill --euid "$USER" --signal SIGINT wf-recorder && exit
+
+# Capture video using slup for screen area
+# timeout and exit after 10 minutes as user has almost certainly forgotten it's running
+timeout 600 wf-recorder -g "$(slurp)" --file=$HOME/Videos/recordings/$(date +"%F_%T").mp4 || exit
